30372e2098f7cfd76d71749c64a7c28227c9090e
Zamarripa <eli.dur.zama@gmail.com>
2023-03-29 13:35:48 -0600

    Initial Commit

---
+++ ProyectoPG/ProyectoPGApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProyectoPGApp.swift
+//  ProyectoPG
+//
+//  Created by CEDAM09 on 29/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ProyectoPGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
